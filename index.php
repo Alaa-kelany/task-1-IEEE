f3412c807e056ec03f2a18f10a57f5d8bc3dab32
Alaa-kelany <113542321+Alaa-kelany@users.noreply.github.com>
2023-02-10 18:07:04 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+echo 10 +20;
+echo '<br>' ;
+echo gettype(10 + 20);
+echo '<br>' ;
+echo 10.5 + 20.8;
+echo '<br>' ;
+echo gettype(10.5 + 20.8);
+echo '<br>' ;
+
+echo 10 - 20;
+echo '<br>' ;
+echo gettype(10 -  20);
+echo '<br>' ;
+echo 10.5 - 20.8;
+echo '<br>' ;
+echo gettype(10.5 - 20.8);
+echo '<br>' ;
+
+echo 10 * 20;
+echo '<br>' ;
+echo gettype(10 * 20);
+echo '<br>' ;
+echo 10.5 * 20.8;
+echo '<br>' ;
+echo gettype(10.5 * 20.8);
+echo '<br>' ;
+
+echo 100 / 20;
+echo '<br>' ;
+echo gettype(100 / 20);
+echo '<br>' ;
+echo 100 / 60;
+echo '<br>' ;
+echo gettype(100 / 60);
+echo '<br>' ;
+
+echo 22 % 10;
+echo '<br>' ;
+
+
+?>
